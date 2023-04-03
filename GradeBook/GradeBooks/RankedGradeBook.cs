4a37320feecb339c32b8a972464b7279c8206253
paulinapiek <piekarska.paulina97@gmail.com>
2023-04-03 22:12:42 +0200

    added RankedGradeBook and StandardGradeBook

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook: BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name) //calling base constructor
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade) //override require virtual 
+        {
+            if (Students.Count()<5)
+            {
+                throw new InvalidOperationException();
+            }
+                return 'F';
+        }
+    }
+}
